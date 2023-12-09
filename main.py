8d26abc29c0434e4ef74c43a70137ff7eb86586b
Devesh-Attri <142967021+Devesh-Attri@users.noreply.github.com>
2023-12-10 00:45:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,120 @@
+import speech_recognition as sr
+import os
+import webbrowser
+import openai
+from config import apikey
+import datetime
+import random
+import numpy as np
+
+def chat(query):
+    global chatStr
+    print(chatStr)
+    openai.api_key = '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'
+    chatStr += f"Devesh: {query}\n Assistant: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt= chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    say(response["choices"][0]["text"])
+    chatStr += f"{response['choices'][0]['text']}\n"
+    return response["choices"][0]["text"]
+
+
+def ai(prompt):
+    openai.api_key = apikey
+    text = f"OpenAI response for Prompt: {prompt} \n *************************\n\n"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    # print(response["choices"][0]["text"])
+    text += response["choices"][0]["text"]
+    if not os.path.exists("Openai"):
+        os.mkdir("Openai")
+
+    # with open(f"Openai/prompt- {random.randint(1, 2343434356)}", "w") as f:
+    with open(f"Openai/{''.join(prompt.split('intelligence')[1:]).strip() }.txt", "w") as f:
+        f.write(text)
+
+def say(text):
+    os.system(f'say "{text}"')
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # r.pause_threshold =  0.6
+        audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from alfred"
+
+if __name__ == '__main__':
+    print("-------------------------------------")
+    print("*-----  Welcome to Alfred A.I  -----*")
+    print("-------------------------------------")
+    hour = datetime.datetime.now().hour
+    if 5 <= hour < 12:
+        say("Good Morning Sir")
+    elif 12 <= hour < 18:
+        say("Good Afternoon Sir")
+    elif 0 <= hour < 5 :
+        say("Good evening sir")
+    else:
+        say("Good Evening Sir")
+    say("Alfred at your service. Please tell me how may I help you?")
+
+    while True:
+        print("Listening...")
+        query = takeCommand()
+        # todo: Add more sites
+        sites = [["youtube", "https://www.youtube.com"], ["Smash Karts", "https://smashkarts.io"], ["Zomato", "https://www.zomato.com"],["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"],]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
+        # todo: Add a feature to play a specific song
+        if "open music" in query:
+            musicPath = "/Users/deveshattri/Documents/Music.mp3"
+            os.system(f"open {musicPath}")
+
+        elif "the time" in query:
+            hour = datetime.datetime.now().strftime("%H")
+            min = datetime.datetime.now().strftime("%M")
+            say(f"Sir time is {hour} hours and {min} minutes")
+
+        elif "open facetime".lower() in query.lower():
+            os.system(f"open /System/Applications/FaceTime.app")
+
+        elif "open whatsapp".lower() in query.lower():
+            os.system(f"open /Users/deveshattri/Desktop/WhatsApp")
+
+        elif "Using artificial intelligence".lower() in query.lower():
+            ai(prompt=query)
+
+        elif "alfred Quit".lower() in query.lower():
+            exit()
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+        else:
+            print("Chatting...")
+            chat(query)
